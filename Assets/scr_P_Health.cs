b7f2e165ccbd6f45797739584717be91404ff550
DOYLE <0501194@Share.BrevardSchools.org>
2016-02-29 14:03:59 -0500

    first commit

---
+++ Assets/scr_P_Health.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scr_P_Health : MonoBehaviour 
+	{
+		//variable declaration
+	    public GameObject myObject;
+		bool alive = true;
+		static int startingHealth = 100;
+		int currentHealth = startingHealth;
+		//end declaration	
+
+		public static void Awake ()
+		{
+		
+		}
+		
+		void Update ()
+		{
+		startingHealth --;
+
+			while (alive = true) {
+
+
+			if (currentHealth <= 0) {			//check if player is alive
+		 		death();
+			}
+			}
+		}
+		public void death(){
+		gameObject.SetActive(false); 			//disable object
+			//Kill Screen
+			//end screen
+			
+			
+		}
+		
+	}
